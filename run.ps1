2d5de08fa236f2b0e546eaaff59bfe6eddaeaa02
VincentGe <cosplox@outlook.com>
2023-11-16 01:14:07 +0800

    Add script to load environment variables and update and run joke executable

---
+++ run.ps1
@@ -0,0 +1,19 @@
+# 读取.env文件内容
+Get-Content .env | ForEach-Object {
+    # 分割键和值
+    $key, $value = $_ -split '=', 2
+
+    # 将变量添加到环境中
+    [System.Environment]::SetEnvironmentVariable($key, $value, [System.EnvironmentVariableTarget]::Process)
+}
+
+# 输出已加载的环境变量
+$env
+
+# update 
+if ($args -contains "-U") {
+    go build -o .\joke\joke.exe .\joke\joke.go
+} 
+
+# run it
+.\joke\joke.exe
